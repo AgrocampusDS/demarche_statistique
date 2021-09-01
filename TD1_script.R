8ab1e9e307b80cd3c7b2fcc76d0e35faeee84fd2
MarieEtienne <MarieEtienne@users.noreply.github.com>
2021-09-01 15:28:05 +0000

    deploy: 77fb1b8446dfd4ad2c879d91e9b756d7baaa0f8e

---
+++ TD1_script.R
@@ -0,0 +1,11 @@
+#############################################################
+##  
+##
+##
+#############################################################
+
+## Importer 'penguins.csv' pour l'afficher à l'écran
+read.table('penguins.csv', sep = ',', header = TRUE)
+
+## Importer 'penguins.csv' pour le stocker dans l'objet penguins
+penguins <- read.table('penguins.csv', sep = ',', header = TRUE)
