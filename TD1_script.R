91d300889ea93bb39355710701884f2c8ac56593
Marie Etienne <marie-pierre.etienne@agrocampus-ouest.fr>
2021-09-09 11:53:34 +0200

    Correction TD1_script.R

--- TD1_script.R
+++ TD1_script.R
@@ -5,7 +5,7 @@
 #############################################################
 
 ## Importer 'penguins.csv' pour l'afficher à l'écran
-read.table('penguins.csv', sep = ',', header = TRUE)
+read.table('penguins.csv', sep = ';', header = TRUE)
 
 ## Importer 'penguins.csv' pour le stocker dans l'objet penguins
-penguins <- read.table('penguins.csv', sep = ',', header = TRUE)
+penguins <- read.table('penguins.csv', sep = ';', header = TRUE)
