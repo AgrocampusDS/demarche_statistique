8e3414e80b5b405b1e8c9d2db59e15abfc0f049e
jlmarchand <jean-louis.marchand@agrocampus-ouest.fr>
2022-08-18 11:37:38 +0200

    correctionducorrrige

--- TD1_script_corrige.R
+++ TD1_script_corrige.R
@@ -8,7 +8,7 @@
 read.table('penguins.csv', sep = ',', header = TRUE)
 
 ## Importer 'penguins.csv' pour le stocker dans l'objet penguins
-penguins <- read.table('penguins.csv', sep = ',', header = TRUE)
+penguins <- read.table('penguins.csv', sep = ';', header = TRUE)
 
 
 summary(penguins)
